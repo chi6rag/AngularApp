85bd750f81343f33e2756e71e258b985dbc798be
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-19 07:41:24 +0530

    Add details view and controller

--- js/controllers.js
+++ js/controllers.js
@@ -10,6 +10,6 @@ artistsControllers.controller('ListController', ['$scope', '$http', function($sc
 artistsControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 	$http.get('js/data.json').success(function(data){
 		$scope.artists = data;
-		
+		$scope.whichItem = $routeParams.itemId;
 	})
 }])
