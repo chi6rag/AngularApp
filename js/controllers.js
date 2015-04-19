bbbaa232cf01c72bcabcd35c380ad72b148c07fd
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-19 07:30:53 +0530

    Setup basic routing for details

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,15 @@
 var artistsControllers = angular.module('artistsControllers', [])
+
 artistsControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
 	$http.get('js/data.json').success(function(data){
 		$scope.artists = data;
 		$scope.artistOrder = 'name';
+	})
+}])
+
+artistsControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('js/data.json').success(function(data){
+		$scope.artists = data;
+		
 	})
 }])
