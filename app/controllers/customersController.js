8dbacedbd50f0a564330fa3a53234d08ecfe888c
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-28 11:43:51 +0530

    Introduce 3 ways of writing controllers

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,4 +1,5 @@
-var myApp = angular.module("myApp", []);
+// Option 1
+/*
 myApp.controller("CustomersController", function($scope){
 	$scope.sortBy = 'name';
 	$scope.reverse = false;
@@ -8,3 +9,32 @@ myApp.controller("CustomersController", function($scope){
 		$scope.reverse = !$scope.reverse;
 	}
 });
+*/
+
+// Option 2
+/*
+(function(){
+	angular.module('customersApp').controller("CustomersController", function($scope){
+		$scope.sortBy = 'name';
+		$scope.reverse = false;
+		$scope.customers=[{joinedDate: '2008-12-06', name: 'Chirag Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2006-12-01', name: 'Rajni Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-03-24', name: 'Palak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-02-01', name: 'Deepak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}];
+		$scope.doSort = function(propName){
+			$scope.sortBy = propName;
+			$scope.reverse = !$scope.reverse;
+		}
+	});
+})();
+*/
+
+// (function(){
+// 	var CustomersController = function($scope){
+// 		$scope.sortBy = 'name';
+// 		$scope.reverse = false;
+// 		$scope.customers=[{joinedDate: '2008-12-06', name: 'Chirag Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2006-12-01', name: 'Rajni Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-03-24', name: 'Palak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-02-01', name: 'Deepak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}];
+// 		$scope.doSort = function(propName){
+// 			$scope.sortBy = propName;
+// 			$scope.reverse = !$scope.reverse;
+// 		}
+// 	}
+// 	angular.module('customersApp').controller("CustomersController", CustomersController);
+// }());
