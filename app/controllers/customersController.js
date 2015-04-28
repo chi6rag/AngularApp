f5963936e0ec7edc06866f0c629de4c9ece1b549
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-28 11:44:36 +0530

    Uncomment files

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -26,15 +26,15 @@ myApp.controller("CustomersController", function($scope){
 })();
 */
 
-// (function(){
-// 	var CustomersController = function($scope){
-// 		$scope.sortBy = 'name';
-// 		$scope.reverse = false;
-// 		$scope.customers=[{joinedDate: '2008-12-06', name: 'Chirag Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2006-12-01', name: 'Rajni Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-03-24', name: 'Palak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-02-01', name: 'Deepak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}];
-// 		$scope.doSort = function(propName){
-// 			$scope.sortBy = propName;
-// 			$scope.reverse = !$scope.reverse;
-// 		}
-// 	}
-// 	angular.module('customersApp').controller("CustomersController", CustomersController);
-// }());
+(function(){
+	var CustomersController = function($scope){
+		$scope.sortBy = 'name';
+		$scope.reverse = false;
+		$scope.customers=[{joinedDate: '2008-12-06', name: 'Chirag Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2006-12-01', name: 'Rajni Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-03-24', name: 'Palak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}, {joinedDate: '2008-02-01', name: 'Deepak Aggarwal', city: 'New Delhi', orderTotal: 9.5975}];
+		$scope.doSort = function(propName){
+			$scope.sortBy = propName;
+			$scope.reverse = !$scope.reverse;
+		}
+	}
+	angular.module('customersApp').controller("CustomersController", CustomersController);
+}());
