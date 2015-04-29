8e3e6bfd7d090a45d4c7eb8f6954ba1d5bb794cc
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-29 12:05:54 +0530

    Add getCustomer method on customersFactory; Use getCustomer method to fetch customer on ordersController

--- app/controllers/ordersController.js
+++ app/controllers/ordersController.js
@@ -2,21 +2,10 @@
 	console.log("loaded orders controller")
 	var OrdersController = function($scope, $routeParams, customersFactory){
 		var customerId = $routeParams.customerId;
-		$scope.orders = null;
-		$scope.customers = []
-
-		function customersInit(){
-			$scope.customers = customersFactory.getCustomers();
-		}
-		customersInit();
+		$scope.customer = null; 
 
 		function init(){
-			for(var i=0, len=$scope.customers.length; i<len; i++){
-				if($scope.customers[i].id === parseInt(customerId)){
-					$scope.orders = $scope.customers[i].orders
-					break;
-				}
-			}
+			$scope.customer = customersFactory.getCustomer(customerId);
 		}
 		init();
 	}
--- app/services/customersFactory.js
+++ app/services/customersFactory.js
@@ -59,9 +59,20 @@
 			}
 		];
 		var factory = {};
+		
 		factory.getCustomers = function(){
 			return customers;
 		};
+
+		factory.getCustomer = function(customerId){
+			for(var i=0, len=customers.length; i<len; i++){
+				if(customers[i].id === parseInt(customerId)){
+					return customers[i]
+				}
+			}
+			return false;
+		}
+
 		return factory;
 	}
 	angular.module('customersApp').factory('customersFactory', customersFactory);
