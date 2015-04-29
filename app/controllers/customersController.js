dcd8ccfd2c140c37504f2be0fff0eb4187843517
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-29 11:38:13 +0530

    Add customersFactory; shift hard coded data from OrdersController and CustomersController to customersFactory

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -30,64 +30,7 @@ myApp.controller("CustomersController", function($scope){
 	var CustomersController = function($scope){
 		$scope.sortBy = 'name';
 		$scope.reverse = false;
-		$scope.customers=[
-			{
-				id: 1,
-				joinedDate: '2008-12-06',
-				name: 'Chirag Aggarwal',
-				city: 'New Delhi',
-				orders:[
-					{
-						id: 1,
-						product: 'Shoes',
-						total: 9.5975
-					}
-				],
-				orderTotal: 9.5975
-			},
-			{
-				id: 2,
-				joinedDate: '2008-12-06',
-				name: 'Palak Aggarwal',
-				city: 'New Delhi',
-				orders:[
-					{
-						id: 23,
-						product: 'Dress',
-						total: 21
-					}
-				],
-				orderTotal: 21
-			},
-			{
-				id: 3,
-				joinedDate: '2008-12-06',
-				name: 'Rajni Aggarwal',
-				city: 'New Delhi',
-				orders:[
-					{
-						id: 99,
-						product: 'Diamond',
-						total: 1000
-					}
-				],
-				orderTotal: 1000
-			},
-			{
-				id: 4,
-				joinedDate: '2008-12-06',
-				name: 'Deepak Aggarwal',
-				city: 'New Delhi',
-				orders:[
-					{
-						id: 4,
-						product: 'T Shirt',
-						total: 500
-					}
-				],
-				orderTotal: 500
-			}
-		];
+		$scope.customers = []
 		$scope.doSort = function(propName){
 			$scope.sortBy = propName;
 			$scope.reverse = !$scope.reverse;
